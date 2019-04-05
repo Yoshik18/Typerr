8c5a0fd564902de23d6a0c6ee6a319c111432a93
Yoshik <yoshikkarnawat@gmail.com>
2019-04-05 14:40:26 +0530

    Second Commit

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-window.addEventListener("load", init);
+document.querySelector("#start").addEventListener("click", init);
 const level = {
   easy: 8,
   medium: 5,
-  hard: 3
+  hard: 3,
+  extreme: 2
 };
 let currentLevel = level.easy;
 let time = currentLevel;
 let score = 0;
 let isPlaying;
+let high;
 
 const seconds = document.querySelector("#seconds");
 const currentWord = document.querySelector("#current-word");
@@ -40,9 +42,11 @@ const words = [
 ];
 function init() {
   showWord(words);
+  wordInput.focus();
   wordInput.addEventListener("input", startMatch);
   setInterval(countDown, 1000);
   setInterval(checkStatus, 50);
+  document.querySelector("#start").style = "display: none";
 }
 function startMatch() {
   if (matchWord()) {
@@ -51,6 +55,7 @@ function startMatch() {
     showWord(words);
     wordInput.value = "";
     score++;
+    highScore();
   }
 
   if (score === -1) {
@@ -63,9 +68,13 @@ function startMatch() {
     currentLevel = level.medium;
     seconds.innerHTML = 4;
   }
-  if (score > 20) {
+  if (score > 19) {
     currentLevel = level.hard;
     seconds.innerHTML = 2;
+  }
+  if (score > 34) {
+    currentLevel = level.extreme;
+    seconds.innerHTML = 1;
   }
 }
 function matchWord() {
@@ -93,11 +102,42 @@ function countDown() {
 }
 function checkStatus() {
   if (!isPlaying && time === 0) {
-    message.innerHTML = `Game Over`;
-    message.style = "color: red";
-    wordInput.style = "border-color: red";
-    score = -1;
-    currentLevel = level.easy;
-    seconds.innerHTML = 7;
+    if (score > 0) {
+      document.querySelector("#high-score").style = "display: block";
+      message.innerHTML = `Game Over!! Please reload the page to start a new game.`;
+      message.style = "color: red";
+      wordInput.style = "border-color: red";
+      score = -1;
+      currentLevel = level.easy;
+      seconds.innerHTML = 7;
+      wordInput.disabled = true;
+    } else if (score === 0) {
+      message.innerHTML = `Game Over!! Please reload the page to start a new game.`;
+      message.style = "color: red";
+      wordInput.style = "border-color: red";
+      score = -1;
+      currentLevel = level.easy;
+      seconds.innerHTML = 7;
+      wordInput.disabled = true;
+      document.querySelector("#high-score").style = "display: none";
+    }
   }
+}
+
+function highScore() {
+  let scores;
+  if (localStorage.getItem("scores") === null) {
+    scores = [];
+  } else {
+    scores = JSON.parse(localStorage.getItem("scores"));
+    high = Math.max(...scores);
+  }
+  scores.push(score);
+  localStorage.setItem("scores", JSON.stringify(scores));
+
+  document.querySelector("#high-score").addEventListener("click", function() {
+    swal("Good job!", `Your High Score Is ${high}`, "success", {
+      button: "Ohh Yeaahh!!"
+    });
+  });
 }
